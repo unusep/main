638df9f123527c61373f8509faf4d74de8a1ff78
Hai Long <e0004824@u.nus.edu>
2016-10-03 07:11:52 +0800

    Create 2 new classes, StartTime and EndTime

---
+++ src/main/java/seedu/address/model/person/EndTime.java
@@ -0,0 +1,9 @@
+package seedu.address.model.person;
+
+/**
+ * Represents an Event's EndTime in the to-do List.
+ * Guarantees: immutable; is valid as declared in {@link #isValidName(String)}
+ */
+public class EndTime {
+
+}
---
+++ src/main/java/seedu/address/model/person/StartTime.java
@@ -0,0 +1,11 @@
+package seedu.address.model.person;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+
+/**
+ * Represents an Event's StartTime in the to-do List.
+ * Guarantees: immutable; is valid as declared in {@link #isValidName(String)}
+ */
+public class StartTime {
+
+}
