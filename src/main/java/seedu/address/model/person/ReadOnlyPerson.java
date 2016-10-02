decb15789e72763cdf7ce3ef6c78097709ba974e
Hai Long <e0004824@u.nus.edu>
2016-10-03 06:12:14 +0800

    morph class ReadOnlyPerson to ReadOnlyEvent

--- src/main/java/seedu/address/model/person/ReadOnlyPerson.java
+++ src/main/java/seedu/address/model/person/ReadOnlyPerson.java
@@ -6,12 +6,12 @@ import seedu.address.model.tag.UniqueTagList;
  * A read-only immutable interface for a Person in the addressbook.
  * Implementations should guarantee: details are present and not null, field values are validated.
  */
-public interface ReadOnlyPerson {
+public interface ReadOnlyEvent {
 
     Name getName();
-    Phone getPhone();
-    Email getEmail();
-    Address getAddress();
+    Description getDescription();
+    StartTime getStartTime();
+    EndTime getEndTime();
 
     /**
      * The returned TagList is a deep copy of the internal TagList,
@@ -22,13 +22,13 @@ public interface ReadOnlyPerson {
     /**
      * Returns true if both have the same state. (interfaces cannot override .equals)
      */
-    default boolean isSameStateAs(ReadOnlyPerson other) {
+    default boolean isSameStateAs(ReadOnlyEvent other) {
         return other == this // short circuit if same object
                 || (other != null // this is first to avoid NPE below
                 && other.getName().equals(this.getName()) // state checks here onwards
-                && other.getPhone().equals(this.getPhone())
-                && other.getEmail().equals(this.getEmail())
-                && other.getAddress().equals(this.getAddress()));
+                && other.getDescription().equals(this.getDescription())
+                && other.getStartTime().equals(this.getStartTime())
+                && other.getEndTime().equals(this.getEndtime()));
     }
 
     /**
@@ -37,29 +37,14 @@ public interface ReadOnlyPerson {
     default String getAsText() {
         final StringBuilder builder = new StringBuilder();
         builder.append(getName())
-                .append(" Phone: ")
-                .append(getPhone())
-                .append(" Email: ")
-                .append(getEmail())
-                .append(" Address: ")
-                .append(getAddress())
-                .append(" Tags: ");
-        getTags().forEach(builder::append);
+                .append(" Description: ")
+                .append(getDescription())
+                .append(" StartTime: ")
+                .append(getStartTime())
+                .append(" EndTime: ")
+                .append(getEndTime())
+                .append(" Category: ")
+                .append(getCategory: ");
         return builder.toString();
     }
-
-    /**
-     * Returns a string representation of this Person's tags
-     */
-    default String tagsString() {
-        final StringBuffer buffer = new StringBuffer();
-        final String separator = ", ";
-        getTags().forEach(tag -> buffer.append(tag).append(separator));
-        if (buffer.length() == 0) {
-            return "";
-        } else {
-            return buffer.substring(0, buffer.length() - separator.length());
-        }
-    }
-
 }
