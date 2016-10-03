b39644fbfd864da11b9fe3e8e07c78a9b634407d
Hai Long <e0004824@u.nus.edu>
2016-10-03 16:31:07 +0800

    Change class Logic and LogicManager after the morph from Person -> Event

--- src/main/java/seedu/address/logic/Logic.java
+++ src/main/java/seedu/address/logic/Logic.java
@@ -2,7 +2,7 @@ package seedu.address.logic;
 
 import javafx.collections.ObservableList;
 import seedu.address.logic.commands.CommandResult;
-import seedu.address.model.event.ReadOnlyPerson;
+import seedu.address.model.event.ReadOnlyEvent;
 
 /**
  * API of the Logic component
@@ -15,7 +15,7 @@ public interface Logic {
      */
     CommandResult execute(String commandText);
 
-    /** Returns the filtered list of persons */
-    ObservableList<ReadOnlyPerson> getFilteredPersonList();
+    /** Returns the filtered list of events */
+    ObservableList<ReadOnlyEvent> getFilteredEventList();
 
 }
