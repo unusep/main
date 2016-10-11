37a919c271da3172bb1e9d59a1ebd3ad21ebc1de
Pu Xiao <xp.xiaopu@gmail.com>
2016-10-11 23:12:30 +0800

    fix #38, fix #19

--- src/test/java/guitests/DeleteCommandTest.java
+++ src/test/java/guitests/DeleteCommandTest.java
@@ -1,54 +1,56 @@
-//package guitests;
-//
-//import org.junit.Test;
-//import seedu.doerList.testutil.TestPerson;
-//import seedu.doerList.testutil.TestUtil;
-//
-//import static org.junit.Assert.assertTrue;
-//import static seedu.doerList.logic.commands.DeleteCommand.MESSAGE_DELETE_PERSON_SUCCESS;
-//
-//public class DeleteCommandTest extends DoerListGuiTest {
-//
-//    @Test
-//    public void delete() {
-//
-//        //delete the first in the list
-//        TestPerson[] currentList = td.getTypicalPersons();
-//        int targetIndex = 1;
-//        assertDeleteSuccess(targetIndex, currentList);
-//
-//        //delete the last in the list
-//        currentList = TestUtil.removePersonFromList(currentList, targetIndex);
-//        targetIndex = currentList.length;
-//        assertDeleteSuccess(targetIndex, currentList);
-//
-//        //delete from the middle of the list
-//        currentList = TestUtil.removePersonFromList(currentList, targetIndex);
-//        targetIndex = currentList.length/2;
-//        assertDeleteSuccess(targetIndex, currentList);
-//
-//        //invalid index
-//        commandBox.runCommand("delete " + currentList.length + 1);
-//        assertResultMessage("The person index provided is invalid");
-//
-//    }
-//
-//    /**
-//     * Runs the delete command to delete the person at specified index and confirms the result is correct.
-//     * @param targetIndexOneIndexed e.g. to delete the first person in the list, 1 should be given as the target index.
-//     * @param currentList A copy of the current list of persons (before deletion).
-//     */
-//    private void assertDeleteSuccess(int targetIndexOneIndexed, final TestPerson[] currentList) {
-//        TestPerson personToDelete = currentList[targetIndexOneIndexed-1]; //-1 because array uses zero indexing
-//        TestPerson[] expectedRemainder = TestUtil.removePersonFromList(currentList, targetIndexOneIndexed);
-//
-//        commandBox.runCommand("delete " + targetIndexOneIndexed);
-//
-//        //confirm the list now contains all previous persons except the deleted person
-//        assertTrue(personListPanel.isListMatching(expectedRemainder));
-//
-//        //confirm the result message is correct
-//        assertResultMessage(String.format(MESSAGE_DELETE_PERSON_SUCCESS, personToDelete));
-//    }
-//
-//}
+package guitests;
+
+import org.junit.Test;
+
+import seedu.doerList.commons.core.Messages;
+import seedu.doerList.logic.commands.DeleteCommand;
+import seedu.doerList.testutil.TestTask;
+import seedu.doerList.testutil.TestUtil;
+
+import static org.junit.Assert.assertTrue;
+
+public class DeleteCommandTest extends DoerListGuiTest {
+
+    @Test
+    public void delete() {
+
+        //delete the first in the list
+        TestTask[] currentList = td.getTypicalTasks();
+        int targetIndex = 1;
+        assertDeleteSuccess(targetIndex, currentList);
+
+        //delete the last in the list
+        currentList = TestUtil.removeTaskFromList(currentList, targetIndex);
+        targetIndex = currentList.length;
+        assertDeleteSuccess(targetIndex, currentList);
+
+        //delete from the middle of the list
+        currentList = TestUtil.removeTaskFromList(currentList, targetIndex);
+        targetIndex = currentList.length / 2;
+        assertDeleteSuccess(targetIndex, currentList);
+
+        //invalid index
+        commandBox.runCommand("delete " + currentList.length + 1);
+        assertResultMessage(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
+
+    }
+
+    /**
+     * Runs the delete command to delete the task at specified index and confirms the result is correct.
+     * @param targetIndexOneIndexed e.g. to delete the first task in the list, 1 should be given as the target index.
+     * @param currentList A copy of the current list of tasks (before deletion).
+     */
+    private void assertDeleteSuccess(int targetIndexOneIndexed, final TestTask[] currentList) {
+        TestTask taskToDelete = currentList[targetIndexOneIndexed - 1]; //- 1 because array uses zero indexing
+        TestTask[] expectedRemainder = TestUtil.removeTaskFromList(currentList, targetIndexOneIndexed);
+
+        commandBox.runCommand("delete " + targetIndexOneIndexed);
+
+        //confirm the list now contains all previous persons except the deleted person
+        assertTrue(taskListPanel.isListMatching(expectedRemainder));
+
+        //confirm the result message is correct
+        assertResultMessage(String.format(DeleteCommand.MESSAGE_DELETE_TASK_SUCCESS, taskToDelete));
+    }
+
+}
