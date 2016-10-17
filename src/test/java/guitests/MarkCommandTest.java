749ff85a762c3b16dd12c86a6b324e87baaeed95
Chan Jing Hui Benedict <chanjhben@hotmail.com>
2016-10-17 21:20:25 +0800

    MarkCommandTest for GUI not tested

--- src/test/java/guitests/MarkCommandTest.java
+++ src/test/java/guitests/MarkCommandTest.java
@@ -1,17 +1,42 @@
 package guitests;
 
+import seedu.doerList.commons.core.Messages;
+import seedu.doerList.logic.commands.MarkCommand;
+import seedu.doerList.model.category.BuildInCategoryList;
 import seedu.doerList.testutil.TestTask;
+
+import static org.junit.Assert.assertTrue;
 
 public class MarkCommandTest extends DoerListGuiTest {
 
     public void mark() {
-        //marks one task
+        //marks one task from top
         TestTask[] currentList = td.getTypicalTasks();
+        int targetIndex = 1;
+        assertMarkSuccess(targetIndex, currentList);
+        
+        //marks one task from middle
+        targetIndex = currentList.length;
+        assertMarkSuccess(targetIndex / 2, currentList);
+        
+        //marks one task from end
+        targetIndex = currentList.length - 1;
+        assertMarkSuccess(targetIndex, currentList);
+        
+        //marks invalid task
+        commandBox.runCommand("mark " + currentList.length + 1);
+        assertResultMessage(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
         
     }
     
-    private void assertMarkSuccess() {
+    private void assertMarkSuccess(int indexToBeMarked, TestTask[] currentList) {
+        TestTask taskToMark = currentList[indexToBeMarked - 1];
         
+        commandBox.runCommand("mark " + indexToBeMarked);
+        
+        assertTrue(taskToMark.getBuildInCategories().contains(BuildInCategoryList.COMPLETE));
+        
+        assertResultMessage(String.format(MarkCommand.MESSAGE_MARK_TASK_SUCCESS, taskToMark));
     }
     
     
