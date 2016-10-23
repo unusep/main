cec54bf2322eb1ac16fb3889ad5eaf60d2538ad8
Hai Long <e0004824@u.nus.edu>
2016-10-23 15:43:55 +0800

    Finish implementing class UndoCommand

--- src/main/java/seedu/doerList/logic/commands/UndoCommand.java
+++ src/main/java/seedu/doerList/logic/commands/UndoCommand.java
@@ -1,16 +1,28 @@
 package seedu.doerList.logic.commands;
 
+import seedu.doerList.logic.UndoRedoManager.DataPair;
+import seedu.doerList.logic.UndoRedoManager.NotUndoableException;
+
 public class UndoCommand extends Command{
 
     public static final String COMMAND_WORD = "undo";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + "Undo the most recent operation that modified the data";
+    public static final String MESSAGE_UNDO_SUCCESS = "Undo the last operation successfully";
+    public static final String MESSAGE_UNDO_FAILURE = "Undo operation is not available in this situation";
 
     public UndoCommand() {}
 
     @Override
     public CommandResult execute() {
-        return null;
+        try {
+            DataPair data = undoRedoManager.getToUndo();
+            model.resetData(data.getList());
+            model.updateFilteredListToShowAll();
+            return new CommandResult(MESSAGE_UNDO_SUCCESS);
+        } catch (NotUndoableException nue) {
+            return new CommandResult(MESSAGE_UNDO_FAILURE);
+        }
     }
 }
