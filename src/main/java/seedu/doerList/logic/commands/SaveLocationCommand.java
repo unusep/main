37b2779e4ab5031723ee3c114e840a31c5de3831
Chan Jing Hui Benedict <chanjhben@hotmail.com>
2016-10-25 00:29:06 +0800

    Updated the saveLocationCommand. It can change the directory now. Loading of directory not done yet.

--- src/main/java/seedu/doerList/logic/commands/SaveLocationCommand.java
+++ src/main/java/seedu/doerList/logic/commands/SaveLocationCommand.java
@@ -1,19 +1,18 @@
 package seedu.doerList.logic.commands;
 
-import java.io.File;
 import java.nio.file.InvalidPathException;
 
 public class SaveLocationCommand extends Command {
     public static final String COMMAND_WORD = "saveto";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Change the save location of your data. "
-            + "Parameters: /s SAVE_LOCATION /n FILE_NAME \n"
+            + "Parameters: /s SAVE_LOCATION [/n FILE_NAME] \n"
             + "Example: " + COMMAND_WORD
             + " /st C:\\Documents\\MyDoerList /n MyNewDoerList";
 
-    public static final String MESSAGE_SUCCESS = "Data saved under: %1$s \nName of file: %2$s";
+    public static final String MESSAGE_SUCCESS = "Data saved under: %1$s";
     public static final String MESSAGE_INVALID_SAVE_LOCATION = "The save location is not found.\n"
-            + "It is either protected or it does not exist.\n"
+            + "It is either protected, does not exist or it contains invalid inputs.\n"
             + "Please enter another location.";
     public static final String DEFAULT_SAVE_FILE_NAME = "MyDoerList";
     
@@ -27,12 +26,13 @@ public class SaveLocationCommand extends Command {
     
     public CommandResult execute() {
         assert model != null;
+        assert storage != null;
         if (fileName == null)
             fileName = DEFAULT_SAVE_FILE_NAME;
         try {
             storage.setSaveLocation(saveLocation);
             model.changeSaveLocation(saveLocation, fileName);
-            return new CommandResult(String.format(MESSAGE_SUCCESS, (Object)(new String[] {saveLocation, fileName})));
+            return new CommandResult(String.format(MESSAGE_SUCCESS, saveLocation));
         } catch (InvalidPathException e) {
             return new CommandResult(String.format(MESSAGE_INVALID_SAVE_LOCATION));
         }
