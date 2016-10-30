9f4e7ec836454ae292b78e329fb238c2844be12d
Jason Scourge <jasonscourge@hotmail.com>
2016-10-30 15:26:49 +0800

    Remove redundant RecurCommand

--- src/main/java/seedu/doerList/logic/parser/Parser.java
+++ src/main/java/seedu/doerList/logic/parser/Parser.java
@@ -88,9 +88,6 @@ public class Parser {
             
         case MarkCommand.COMMAND_WORD:
             return prepareMark(arguments);
-            
-        case RecurCommand.COMMAND_WORD:
-            return prepareRecur(arguments);
 
         case TaskdueCommand.COMMAND_WORD:
             return new TaskdueCommand(arguments.trim());
@@ -312,22 +309,6 @@ public class Parser {
         }
 
         return new MarkCommand(index.get());
-    }
-    
-    /**
-     * Parses arguments in the context of the Recur task command.
-     *
-     * @param args full command args string
-     * @return the prepared command
-     */
-    private Command prepareRecur(String args) {
-        Optional<Integer> index = parseIndex(args);
-        if(!index.isPresent()){
-            return new IncorrectCommand(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, RecurCommand.MESSAGE_USAGE));
-        }
-
-        return new RecurCommand(index.get());
     }
     
     /**
